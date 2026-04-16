5c90f99edf056961624431951e3e9c33e51fc53c
Yuliia <ciceron131313@gmail.com>
2026-04-17 01:12:20 +0300

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -69,3 +69,15 @@ export const register = async (data: RegisterRequest) => {
    const res = await nextServer.post<User>('/auth/register', data);
    return res;
 }
+
+
+
+export type LoginRequest = {
+  email: string;
+  password: string;
+};
+
+export const login = async (data: LoginRequest) => {
+   const res = await nextServer.post<User>('/auth/login', data);
+   return res.data;
+};
