3e39e3c9b09f896124f50c748d58de7b0c3b28e4
Yuliia <ciceron131313@gmail.com>
2026-04-25 02:19:37 +0300

    Update page.tsx

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,19 +1,12 @@
 import ProfilePage from '@/components/ProfilePage/ProfilePage';
 import { getServerMe } from '@/lib/api/serverApi';
-import { Metadata } from 'next';
 
-export async function generateMetadata(): Promise<Metadata> {
-
-  const user = await getServerMe()
-
-    return {
-      title: `Profile: ${user.username}`,
-      description: `User Email: ${user.email}`,
-    };
- }
 
 const Profile = async () => {
-    const user = await getServerMe()
+  const user = await getServerMe();
+
+  console.log('USER', user);
+  
   return <ProfilePage user={user}/>
 
 };
