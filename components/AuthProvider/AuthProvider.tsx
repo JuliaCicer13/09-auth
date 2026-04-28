fa0b3af4f61484bda6f4ab0675cb8f29ce991930
Yuliia <ciceron131313@gmail.com>
2026-04-28 21:15:54 +0300

    Update AuthProvider.tsx

--- components/AuthProvider/AuthProvider.tsx
+++ components/AuthProvider/AuthProvider.tsx
@@ -1,41 +1,69 @@
 'use client';
 
-import { checkSession, getMe } from "@/lib/api/clientApi";
+import { getMe } from "@/lib/api/clientApi";
+
 import { useAuthStore } from "@/lib/store/authStore";
-import { useEffect , useState} from "react";
+
+import { useEffect, useState } from "react";
+
 import Spinner from "../Loader/loader";
 
 type Props = {
-    children: React.ReactNode;
+
+  children: React.ReactNode;
+
 };
 
-const AuthProvider = ({children}: Props) => {
-    const [loading, setLoading] = useState(true);
+const AuthProvider = ({ children }: Props) => {
 
-    const setUser = useAuthStore((state) => state.setUser);
-    const clearIsAuthenticated = useAuthStore((state) => 
-  state.clearIsAuthenticated);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-     const fetchUser = async () => {
-        try{
-        const isAuthenticated = await checkSession();
-        if (isAuthenticated) {
-             const user = await getMe();
-            if (user) setUser(user);
+  const setUser = useAuthStore((state) => state.setUser);
+
+  const clearIsAuthenticated = useAuthStore(
+
+    (state) => state.clearIsAuthenticated
+
+  );
+
+  useEffect(() => {
+
+    const fetchUser = async () => {
+
+      try {
+
+        const user = await getMe(); // 👈 сразу берём юзера
+
+        if (user) {
+
+          setUser(user);
+
         } else {
-            clearIsAuthenticated();
+
+          clearIsAuthenticated();
+
         }
-     } finally {
-        setLoading(false)
-     }
-   };
-     fetchUser();
-    },  [setUser, clearIsAuthenticated]);
 
-    if (loading) return <Spinner/>
+      } catch {
 
-    return children;
+        clearIsAuthenticated();
+
+      } finally {
+
+        setLoading(false);
+
+      }
+
+    };
+
+    fetchUser();
+
+  }, [setUser, clearIsAuthenticated]);
+
+  if (loading) return <Spinner />;
+
+  return children;
+
 };
 
 export default AuthProvider;
